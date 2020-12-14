4ceedd595dff223307febb8c18b574d0f87ccd96
Lucas Farias <lucasnfarias01@gmail.com>
2020-12-13 21:50:58 -0300

    creating vue router file

---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import CommenList from '../pages/CommentList';
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: CommenList
+  },
+  {
+    path: '/dev-infos',
+    name: 'DevInfos',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../pages/DevInfos')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
